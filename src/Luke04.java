03fef0118095e6a000f547430b4ecbb81519b437
Eivind Bergstøl <eivind@bergstol.no>
2014-12-04 18:10:59 +0100

    Refaktorert litt. Boy scout style.

--- src/Luke04.java
+++ src/Luke04.java
@@ -4,8 +4,6 @@ import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.net.URLConnection;
-import java.util.function.Supplier;
-import java.util.stream.Stream;
 
 import static java.lang.Double.parseDouble;
 
@@ -14,30 +12,33 @@ public class Luke04 {
     public static void main(String[] args) throws URISyntaxException, IOException {
         Luke04 luke = new Luke04();
 
-        String file = "https://dl.dropboxusercontent.com/u/45621/kilma_data_blindern.txt";
-        URLConnection connection = new URL(file).openConnection();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-        Supplier<Stream<Temp>> supplier =
-                () -> reader.lines()
-                        .filter(s -> luke.filterWeatherDataBlindern(s))
-                        .map(s -> new Temp(s))
-                        .filter(temp -> luke.filterDesember(temp.Dato));
-
-        Temp temp1 = supplier.get()
+        Temp laveste = luke.reader.lines()
+                .filter(s -> luke.filterWeatherDataBlindern(s))
+                .map(s -> new Temp(s))
+                .filter(temp -> luke.filterDesember(temp.Dato))
                 .reduce((t1, t2) -> t1.TAN < t2.TAN ? t1 : t2).get();
 
-        System.out.println("Laveste temp: " + temp1);
+        System.out.println("Laveste temp: " + laveste);
+    }
+
+    public final BufferedReader reader;
+
+
+    public Luke04() throws IOException {
+        String file = "https://dl.dropboxusercontent.com/u/45621/kilma_data_blindern.txt";
+        URLConnection connection = new URL(file).openConnection();
+        this.reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
     }
 
     private boolean filterWeatherDataBlindern(String s) {
         return s.startsWith("   18700 ");
     }
-    private boolean filterDesember(String s){
+
+    private boolean filterDesember(String s) {
         return s.split("\\.")[1].equals("12");
     }
 
-    public static class Temp{
+    public static class Temp {
 
         public final double TAN;
         public final String Dato;
