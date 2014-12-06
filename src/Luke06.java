701312d6fb56f33081fc17a17a4d3d10187605c8
Eivind Bergstøl <eivind@bergstol.no>
2014-12-06 18:32:20 +0100

    Oppdatert etter input fra kolleger med hjernebarken intakt.

--- src/Luke06.java
+++ src/Luke06.java
@@ -1,19 +1,21 @@
-import java.util.HashSet;
-import java.util.Set;
-
 public class Luke06 {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        Set<Integer> products = new HashSet<>();
+        boolean[] myset = new boolean[(8000 + 1) * (8000 + 1)];
 
-        for(int i = 1; i <= 8000; i++){
-            for(int p = 1; p <= 8000; p++){
-                products.add(i*p);
+        for (int i = 1; i <= 8000; i++) {
+            for (int p = i; p <= 8000; p++) {
+                myset[i * p] = true;
             }
         }
+        int count = 0;
+        for (int i = 0; i < myset.length; i++) {
+            if (myset[i]) count++;
+        }
 
-        System.out.println("Antall: " + products.size() + " " + (System.currentTimeMillis() - start) + "ms");
+
+        System.out.println("Antall: " + count + " " + (System.currentTimeMillis() - start) + "ms");
 
     }
 }
